7c6813f8bbf65e69f5de8b56352bed1690faf1fd
Joel Mclemore <jmclemore3@mail.valenciacollege.edu>
2016-01-27 18:57:01 -0500

    Program completed!
    In English, In German, and Russian!

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
-# replace the contents of this comment with your full name
+# Joel Mclemore
 
 # write a program that:
 # 1. greets the user in English
@@ -7,4 +7,24 @@
 # 4. exits
 
 # make sure that your code contains comments explaining your logic!
-
+while True: #While loop to prompt the user andd keep operations contained
+	print('Hello! Enter Language type -- EN, DE, RU') #3 language types to choose from
+	lang = input()
+	if lang == 'EN, DE, RU':
+			continue  #continue with the loop
+	if lang == "EN": #statement for english users
+	  print('Please type your name')
+	  name = input()
+	  print('Hello,',name ) #To be honest prof. Alfaro, I remember C# having a similar functionality and got lucky this time.
+	  break
+	if lang == "DE":
+	  print('Bitte dein namen schreiben')
+	  name = input()
+	  print('Hallo,',name ) #c/p from line 9-11, auf Deutsch
+	  break
+	if lang == "RU":
+	  print('Пожалуйста писать ваше имя')
+	  name = input()
+	  print('Привет,',name ) #c/p from 9-11, Pyccknñ
+	  break
+	exit
