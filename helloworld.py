5061d880ccb52bf534e660269148d3e6063875b4
Joel Mclemore <jmclemore3@mail.valenciacollege.edu>
2016-01-28 20:30:45 -0500

    Change
    Added a proper exit

--- helloworld.py
+++ helloworld.py
@@ -8,23 +8,25 @@
 
 # make sure that your code contains comments explaining your logic!
 while True: #While loop to prompt the user andd keep operations contained
-	print('Hello! Enter Language type -- EN, DE, RU') #3 language types to choose from
+	print('Hello! To continue, type "1" for EN,"2" for DE, "3" for RU. Type "4" to exit') #3 language types to choose from
 	lang = input()
-	if lang == 'EN, DE, RU':
+	if lang == '1, EN, 2, DE, 3, RU':
 			continue  #continue with the loop
-	if lang == "EN": #statement for english users
+	if lang == "1": #statement for english users
 	  print('Please type your name')
 	  name = input()
 	  print('Hello,',name ) #To be honest prof. Alfaro, I remember C# having a similar functionality and got lucky this time.
 	  break
-	if lang == "DE":
+	if lang == "2":
 	  print('Bitte dein namen schreiben')
 	  name = input()
 	  print('Hallo,',name ) #c/p from line 9-11, auf Deutsch
 	  break
-	if lang == "RU":
+	if lang == "3":
 	  print('Пожалуйста писать ваше имя')
 	  name = input()
 	  print('Привет,',name ) #c/p from 9-11, Pyccknñ
 	  break
-	exit
+	if lang == "4":
+	  break
+exit
